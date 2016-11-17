f5a8e1e4609d370c66c74e77fb9204c8d12c8929
EGAguirre <9eaguirre@stpatrick.org>
2016-11-17 10:54:22 -0600

    added a lot.
    -added class
    -added instance variabes and functions

---
+++ pizza.js
@@ -0,0 +1,22 @@
+class pizza{
+  //type constructor below
+constructor(s,mt,vt){
+  this.size = s; // array["s","m","l","xl"]
+  this.meatToppings = mt; //
+  this.veggieToppings = vt;
+}
+
+
+  //type instance functions below
+sizeCost(){
+  let cost=0;
+  if(this.size == "s")
+    cost=7.99;
+  }
+}
+
+
+  //type class functions below
+
+
+}
